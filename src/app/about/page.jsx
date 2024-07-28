3d9f08e7feeb649c7f25cf380e1324b97fb2cb86
Eric <e.k.chi7@gmail.com>
2024-07-28 18:47:14 -0400

    added styling to about page but running into error with image that should be only taking up the right half of the page

--- src/app/about/page.jsx
+++ src/app/about/page.jsx
@@ -3,9 +3,36 @@ import styles from "./about.modules.css"
 
 const AboutPage = () => {
     return (
-        <div>
+        <div className={styles.container}>
+            <div className={styles.textContainer}>
+                <h2 className={styles.subtitle}>About Agency</h2>
+                <h1 className={styles.title}>We create digital ideas that are bigger, bolder, braver and better.</h1>
+                <p className={styles.desc}>We create digital ideas that are bigger bolder, braver and better.
+                    We believe in good ideas flexibility and precision.
+                    We're the the best consulting and finance solution provider.
+                    Wide range of web and software development services.
+                </p>
+                <div className={styles.boxes}>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                    <div className={styles.box}>
+                        <h1>10 K+</h1>
+                        <p>Year of experience</p>
+                    </div>
+                </div>
+            </div>
             <div className={styles.imgContainer}>
-                <Image src="https://images.pexels.com/photos/24460824/pexels-photo-24460824/free-photo-of-esb-among-lower-skyscrapers.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill/>
+                <Image
+                src="/about.png"
+                alt="About Image"
+                fill
+                />
             </div>
         </div>
     )
