1e14cca1f56dd798571a08477fcdfc3bb0bc03da
Eric <e.k.chi7@gmail.com>
2024-07-28 18:01:36 -0400

    styling homepage

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,23 @@
+import styles from "./home.module.css"
+import Image from "next/image";
+
 const Home = () => {
-  throw new Error('Error in Home');
-  return <div>Homepage</div>;
+  return <div className={styles.container}>\
+    <div className={styles.textContainer}>
+      <h1>Creative Thoughts Agency.</h1>
+      <p>Lorem ipsumsahsdfasdjfahsdkjfhaldskjajdsl</p>
+      <div className={styles.buttons}>
+        <button className={styles.button}>Learn More</button>
+        <button className={styles.button}>Contact</button>
+      </div>
+      <div className={styles.brands}>
+        <Image src="/brands.png" alt="" fill className={styles.brandImg}/>
+      </div>
+    </div>
+    <div className={styles.imgContainer}>
+    <Image src="/hero.gif" alt="" fill className={styles.heroGif}/>
+    </div>
+  </div>;
 };
 
 export default Home;
