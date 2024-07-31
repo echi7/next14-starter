98e0058f13678b50691f261d2b11906436240ecc
Eric <e.k.chi7@gmail.com>
2024-07-31 08:56:45 -0400

    creating hte html for the single posts

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,6 +1,23 @@
+import styles from "./singlePost.module.css"
+
 const SinglePostPage = () => {
     return (
-        <div>SinglePostPage</div>
+        <div className={styles.container}>
+            <div className={styles.imgContainer}>
+                <Image src="https://images.pexels.com/photos/13070618/pexels-photo-13070618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill className={styles.img}/>
+                <div className={styles.detailText}>
+                    <span className={styles.detailTitle}>Author</span>
+                    <span className={styles.detailValue}>Terry Jefferson</span>
+                </div>
+                <div className={styles.detailText}>
+                    <span className={styles.detailTitle}>Published</span>
+                    <span className={styles.detailValue}>01.01.2024</span>
+                </div>
+            </div>
+            <div className={styles.content}>
+                random sentence generated for this single post.
+            </div>
+        </div>
     )
 }
 
