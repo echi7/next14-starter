3f43c746e55d5f783c7d5af051145575008f4511
Eric <e.k.chi7@gmail.com>
2024-07-31 12:07:57 -0400

    changing data file so that it is not hard coded and instead we are pulling from mongodb

--- src/lib/data.js
+++ src/lib/data.js
@@ -1,24 +1,47 @@
-const users = [
-    {id:1, name:'John'},
-    {id:2, name:'Jane'},
-]
+import { Post } from "./models";
+import { connectToDb } from "./utils";
 
-const posts = [
-    {id:1, title:"post 1", body:"...0..", userId: 1},
-    {id:2, title:"post 2", body:"..1...", userId: 1},
-    {id:3, title:"post 3", body:"...2..", userId: 2},
-    {id:4, title:"post 4", body:"...3..", userId: 2},
-]
+export const getPosts = async () => {
+    try {
+        connectToDb();
+        const posts = await Post.find()
 
-export const getPosts = () => {
-    return posts;
+        return posts
+    }catch(error){
+        console.log(error)
+        throw new Error("Failed to fetch posts!")
+    }
 };
+export const getPost = async (slug) => {
+    try {
+        connectToDb();
+        const post = await Post.find({slug})
 
-export const getPost = async (id) => {
-    const post = posts.find((post) => post.id === parseInt(id));
-    return post;
+        return post;
+    }catch(error){
+        console.log(error)
+        throw new Error("Failed to fetch post!")
+    }
 };
-
 export const getUser = async (id) => {
-    return users.find((user) => user.id === parseInt(id));
+    try {
+        connectToDb();
+        const user = await Post.find({id})
+
+        return user
+    }catch(error){
+        console.log(error)
+        throw new Error("Failed to fetch user!")
+    }
+};
+export const getUsers = async () => {
+    try {
+        connectToDb();
+        const users = await Post.find()
+
+        return users
+    }catch(error){
+        console.log(error)
+        throw new Error("Failed to fetch users!")
+    }
 };
