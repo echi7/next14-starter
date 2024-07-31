094dd5a11c864ac32259e956797f10c05ce45bd3
Eric <e.k.chi7@gmail.com>
2024-07-31 09:08:32 -0400

    adding styling to the single post page

--- src/app/blog/[slug]/page.jsx
+++ src/app/blog/[slug]/page.jsx
@@ -1,10 +1,27 @@
 import styles from "./singlePost.module.css"
+import Image from "next/image"
 
 const SinglePostPage = () => {
     return (
         <div className={styles.container}>
             <div className={styles.imgContainer}>
-                <Image src="https://images.pexels.com/photos/13070618/pexels-photo-13070618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2" alt="" fill className={styles.img}/>
+                <Image className={styles.img}
+                src="https://images.pexels.com/photos/13070618/pexels-photo-13070618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                alt=""
+                fill
+                />
+                </div>
+                <div className={styles.textContainer}>
+                    <h1 className={styles.title}>Title</h1>
+                    <div className={styles.detail}>
+                        <Image
+                        className={styles.avatar}
+                        src="https://images.pexels.com/photos/13070618/pexels-photo-13070618.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                        alt=""
+                        width={50}
+                        height={50}
+                        />
+                    </div>
                 <div className={styles.detailText}>
                     <span className={styles.detailTitle}>Author</span>
                     <span className={styles.detailValue}>Terry Jefferson</span>
