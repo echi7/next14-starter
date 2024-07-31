1675842a0986b6534f9842c9a94bce2ad7b12f1c
Eric <e.k.chi7@gmail.com>
2024-07-31 11:31:20 -0400

    changing library to be able to get posts

--- src/app/blog/page.jsx
+++ src/app/blog/page.jsx
@@ -1,19 +1,10 @@
 import PostCard from '@/components/postCard/postCard'
 import styles from './blog.module.css'
 
-const getData = async () => {
 
-    const res = await fetch("https://jsonplaceholder.typicode.com/posts", {next:{revalidate: 3600}});
-
-    if (!res.ok){
-        throw new Error("Something is wrong")
-    }
-
-    return res.json()
-}
 
 const BlogPage = async () => {
-    const posts = await getData()
+
     return (
         <div className={styles.container}>
         {posts.map((post) => (
---
+++ src/lib/data.js
@@ -0,0 +1,23 @@
+const users = [
+    {id:1, name:'John'},
+    {id:2, name:'Jane'},
+]
+
+const posts = [
+    {id:1, title:"post 1", body:"...0..", userId: 1},
+    {id:2, title:"post 2", body:"..1...", userId: 1},
+    {id:3, title:"post 3", body:"...2..", userId: 2},
+    {id:4, title:"post 4", body:"...3..", userId: 2},
+]
+
+export const getPosts = () => {
+
+};
+
+export const getPost = async (id) => {
+    return posts.find((post) => post.id === id);
+};
+
+export const getUser = async (id) => {
+    return user.find((user) => user.id === id);
+};
