312b6a5f3aad4536213d94018eecf82612f97a81
Eric <e.k.chi7@gmail.com>
2024-08-03 19:39:14 -0400

    creating an action and form data and connecting to the db to create a new post

--- src/app/serveactiontest/page.jsx
+++ src/app/serveactiontest/page.jsx
@@ -1,18 +1,14 @@
-import { sayHello } from "@/lib/actions"
+import { addPost } from "@/lib/actions"
 
 const ServerActionTestPage = () => {
-
-    const actionincomponent = async () => {
-        "use server"
-        console.log("itworks!")
-    }
     return (
         <div>
-            <form action={actionincomponent}>
-                <input type="text" placeholder="title"/>
-                <input type="text" placeholder="desc"/>
-                <input type="text" placeholder="slug"/>
-                <button>Test me</button>
+            <form action={addPost}>
+                <input type="text" placeholder="title"name="title"/>
+                <input type="text" placeholder="desc"name="desc"/>
+                <input type="text" placeholder="slug"name="slug"/>
+                <input type="text" placeholder="userId"name="userId"/>
+                <button>Create</button>
             </form>
         </div>
     )
--- src/lib/actions.js
+++ src/lib/actions.js
@@ -1,5 +1,20 @@
-export const addPost = async () => {
+import { connectToDb } from "./utils";
+
+export const addPost = async (formData) => {
     "use server"
 
-    console.log("hello")
-}
+    const { title, desc, slug, userId } = Object.fromEntries(formData);
+
+    try{
+        connectToDb()
+        const newPost = new Post({
+            title,
+            desc,
+            slug,
+            userId,
+        });
+    } catch(error) {
+        console.log('something went wrong')
+        return {error: "something went wrong"}
+    }
+};
